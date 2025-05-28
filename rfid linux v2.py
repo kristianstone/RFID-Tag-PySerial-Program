f151faba2b3495966f3340554b241486509a02b4
kristian <kvkstone1@gmail.com>
2025-05-28 10:28:31 +1000

    functional across both lanes

--- rfid linux v2.py
+++ rfid linux v2.py
@@ -20,13 +20,15 @@ currentRFID2 = "test"
 
 # create reader - this should make it easier having two readers
 reader1 = Reader(False, "empty") # initalize first reader
-#reader2 = Reader(False, "empty") # second reader
+reader2 = Reader(False, "empty") # second reader
+
+# may eventually use this properly, but for now just using global variables
 vidLane1 = Reader(False, "empty") # VID detector lane 1
 vidLane2 = Reader(False, "empty") # VID detector lane 2
 
 # queue creation
 queue1 = queue.Queue() # queue for reader 1
-#queue2 = queue.Queue() # queue for reader 2
+queue2 = queue.Queue() # queue for reader 2
 queue3 = queue.Queue() # queue for VID detector
 
 '''
@@ -36,7 +38,7 @@ Serial Port Allocations
 ser1 = serial.Serial('/dev/ttyUSB0', baudrate=9600) #open serial port default 8N1
 
 # reader 2 - port 2 - COM7 on Windows - /dev/ttyUSB1 on Linux
-#ser2 = serial.Serial('/dev/ttyUSB1', baudrate=9600) #open serial port default 8N1
+ser2 = serial.Serial('/dev/ttyUSB1', baudrate=9600) #open serial port default 8N1
 
 # VID detector input - port 3 - /dev/ttyUSB2 on Linux
 ser3 = serial.Serial('/dev/ttyUSB2', baudrate=9600)
@@ -51,18 +53,17 @@ def serial_read(s, readerName):
         sline = s.readline()
         if readerName == "R1:": # add to reader 1 queue
             queue1.put(sline.decode('utf-8'))
-        #else: # add to reader 2 queue
-            #queue2.put(readerName + sline.decode('utf-8')) # may consider bringing readerName back
+        elif readerName == "R2:": # add to reader 2 queue
+            queue2.put(sline.decode('utf-8')) # may consider bringing readerName back
         else: # add to VID queue
             queue3.put(sline.decode('utf-8'))
 
-
 # creating each thread to receive data from readers
 r1 = threading.Thread(target=serial_read, args=(ser1, "R1:",)).start() # reader 1 thread
-#r2 = threading.Thread(target=serial_read, args=(ser2, "R2:",)).start() # reader 2 thread
+r2 = threading.Thread(target=serial_read, args=(ser2, "R2:",)).start() # reader 2 thread
 vid = threading.Thread(target=serial_read, args=(ser3, "VID",)).start() # VID detector thread
 
-print("Ready") # This indicates that the software is ready to begin
+print("Ready") # This indicates that the software is ready
 
 # need to create out text file too
 
@@ -80,16 +81,25 @@ while True:
         reader1.change_tag(queue1.get(True))
         # conversion to the proper string, look up table handled inside of reader class
         currentRFID1 = "1-BBT" + reader1.get_fleetNumber(csvFleetList) + ",00000000" + '\n' # not sure if new line required for final build
-        #ser3.write(muxString.encode('utf-8')) # testing string to transmit from port 4 over rs232
-        print("RFID Read: " + repr(currentRFID1)) # print the current RFID for testing purposes
+        print("RFID Lane 1 Read: " + repr(currentRFID1)) # print the current RFID for testing purposes
 
     # for reader 2 - need to basically duplicate reader 1 
     '''
     # reader 2 processing 
 
-    2-BBT 
+    2-BBT
+    
+    Probably should be a function to handle this
     '''
+    if queue2.empty():
+        reader2.change_tag("empty")
+        currentRFID2 = "empty"
+    else:
+        reader2.change_tag(queue2.get(True))
+        currentRFID2 = "2-BBT" + reader2.get_fleetNumber(csvFleetList) + ",00000000" + '\n'
+        print("RFID Lane 2 Read: " + repr(currentRFID2)) # repr to show escape characters like \n
 
+    
     
     # VID detector reads for each lane
     vid_input = None
@@ -103,19 +113,14 @@ while True:
         currentVID2 = "empty"
     elif vid_input[0] == "1":
         currentVID1 = vid_input
-        print("VID1 Read: " + repr(currentVID1))
+        print("VID Lane 1 Read: " + repr(currentVID1))
     elif vid_input[0] == "2":
         currentVID2 = vid_input
-        print("VID2 Read: " + repr(currentVID2))
+        print("VID Lane 2 Read: " + repr(currentVID2))
 
 
     # need to determine a method if one lane is empty and the other is not
-
-    # test purpose code - comparison of strings
     
-    # store both RFID and VID detector strings in a txt file
-    # compare RFID and VID strings and update a CSV
-    # need comparison for each lane
 
     # lane 1 comparison - should use get tag for this too
     if currentVID1 == currentRFID1 and currentVID1 != "empty" and currentRFID1 != "empty": # ensure not empty so that nothing is printed either
@@ -127,16 +132,25 @@ while True:
         time.sleep(1)
     
     # lane 2 comparison
-
+    if currentVID1 == currentRFID1 and currentVID1 != "empty" and currentRFID1 != "empty": # ensure not empty so that nothing is printed either
+        print("yes") 
+        print("Output to PLC: " + repr(currentVID1)) # for trial, output only VID detector string
+        time.sleep(1)  # sleep for 1 second to allow for output to PLC
+    else:
+        print("no")
+        time.sleep(1)
+    
     # logic for storing data to analyse later
 
 # need to parse VID no matter what
+#ser3.write(muxString.encode('utf-8')) # testing string to transmit from port 4 over rs232
 
     # true or false if results match for lane 1
     matchresult1 = currentVID1 == currentRFID1 and currentVID1 != "empty" and currentRFID1 != "empty"
 
     # want to store entire string with repr
     # only write when there is a read from RFID or VID
+    # might create a function to handle this and take the lane as an argument
     if currentVID1 != "empty" or currentRFID1 != "empty":
         with open(resultsFile, 'a', newline='') as csvfile:
             fieldnames = ['timestamp', 'lane', 'vid', 'rfid', 'rfidNum', 'match']
@@ -154,5 +168,26 @@ while True:
                 'rfid': repr(currentRFID1),
                 'rfidNum': reader1.get_tag(),
                 'match': matchresult1
+            })
+
+    # true or false if results match for lane 2
+    matchresult2 = currentVID2 == currentRFID2 and currentVID2 != "empty" and currentRFID2 != "empty"
+    if currentVID2 != "empty" or currentRFID2 != "empty":
+        with open(resultsFile, 'a', newline='') as csvfile:
+            fieldnames = ['timestamp', 'lane', 'vid', 'rfid', 'rfidNum', 'match']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            # write header if file is empty
+            if csvfile.tell() == 0:
+                writer.writeheader()
+
+            # write the current results
+            writer.writerow({
+                'timestamp': now.strftime('%Y-%m-%d %H:%M:%S'),
+                'lane': '2',
+                'vid': repr(currentVID2),
+                'rfid': repr(currentRFID2),
+                'rfidNum': reader2.get_tag(),
+                'match': matchresult2
             })
 
--- vidOutputSim.py
+++ vidOutputSim.py
@@ -3,14 +3,15 @@ import datetime as dt
 import time
 from rfidClasses import *
 
-
 '''
 Serial Port Allocations
 '''
 # VID detector input - port 3 - /dev/ttyUSB2 on Linux or COM15 on Windows
 ser3 = serial.Serial('COM16', baudrate=9600)
 line = "1-BBT2809,00000000" + '\n' # new line is temp
+line2 = "2-BBT2809,00000000" + '\n'
 
 while True:
     ser3.write(line.encode('utf-8'))
+    ser3.write(line2.encode('utf-8'))
     time.sleep(1)
